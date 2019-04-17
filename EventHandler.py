1a7397adb5f52817e21b0f5961970bb7fa060f37
bin4rym4ge <bin4rym4ge@users.noreply.github.com>
2019-04-17 11:27:45 -0700

    KEY Flags part 1

--- EventHandler.py
+++ EventHandler.py
@@ -3,7 +3,35 @@
 class EventHandler(object):
     def __init__(self):
         self.Flag = {
-        "init": 0
+        "init": 0,
+        "KEY_A": 0,
+        "KEY_B": 0,
+        "KEY_C": 0,
+        "KEY_D": 0,
+        "KEY_E": 0,
+        "KEY_F": 0,
+        "KEY_G": 0,
+        "KEY_H": 0,
+        "KEY_I": 0,
+        "KEY_J": 0,
+        "KEY_K": 0,
+        "KEY_L": 0,
+        "KEY_M": 0,
+        "KEY_N": 0,
+        "KEY_O": 0,
+        "KEY_P": 0,
+        "KEY_Q": 0,
+        "KEY_R": 0,
+        "KEY_S": 0,
+        "KEY_T": 0,
+        "KEY_U": 0,
+        "KEY_V": 0,
+        "KEY_W": 0,
+        "KEY_X": 0,
+        "KEY_Y": 0,
+        "KEY_Z": 0,
+        "KEY_SPACE": 0,
+        "KEY_ENTER": 0
         }
 
 
